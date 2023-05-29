51c657f8e6fc2193b3a1b37f2a8eb1d9a2eb5742
enrikku <91900904+enrikku@users.noreply.github.com>
2023-05-29 16:35:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const archivo = './film.txt';
+  const archivo = "./film.txt";
 
-    fetch(archivo)
-      .then(response => response.text())
-      .then(contenido => {
-        const lineas = contenido.split('\n');
-        
-        // console.log(lineas[2]);
-        
-        lineas.forEach(linea => {
-        
-            const nombre = linea.trim();
-    
-            //console.log(nombre);
+  fetch(archivo)
+    .then((response) => response.text())
+    .then((contenido) => {
+      const lineas = contenido.split("\n");
 
+      lineas.forEach((linea) => {
+        const nombre = linea.trim();
 
+        if (nombre !== "") {
+          const divContainer = document.getElementById("container");
+          const divFilm = document.createElement("div");
+          const divImage = document.createElement("div");
+          const divText = document.createElement("div");
+          const titol = document.createElement("h3");
+          const nomClassImage = nombre.replace(/ /g, "-");
+          const rutaImg = "./img/" + nomClassImage + ".jpg";
+          divFilm.id = "film";
 
-          if (nombre !== '') {
-            const divContainer = document.getElementById('container')
-            const divFilm = document.createElement('div');
-            const divImage = document.createElement('div');
-            const divText = document.createElement('div');
-            const titol = document.createElement('h3');
-            const nomClassImage = nombre.replace(/ /g,"-")
-            const rutaImg = './img/' + nomClassImage + '.jpg';
-            divFilm.id = 'film';
+          divImage.id = "film-image";
+          divImage.classList = nombre;
 
-            divImage.id = 'film-image'
-            divImage.classList = nombre;
-            
-            divImage.style.backgroundImage = `url(${rutaImg})`;
-            divImage.style.backgroundSize = '100% 100%';
+          divImage.style.backgroundImage = `url(${rutaImg})`;
+          divImage.style.backgroundSize = "100% 100%";
 
-            divText.id = 'text'
-            titol.textContent = nombre;
-            
-            divContainer.appendChild(divFilm);
-            divFilm.appendChild(divImage);
-            divImage.appendChild(divText)
-            divFilm.appendChild(divText);
-            divText.appendChild(titol);
-          }
-        });
-      })
-      .catch(error => console.error(error));
-    
+          divText.id = "text";
+          titol.textContent = nombre;
+
+          divContainer.appendChild(divFilm);
+          divFilm.appendChild(divImage);
+          divImage.appendChild(divText);
+          divFilm.appendChild(divText);
+          divText.appendChild(titol);
+
+          var total = document.getElementById("total")
+          var qtt = document.querySelectorAll("#film");
+
+          total.textContent = qtt.length
+        }
+      });
+    })
+    .catch((error) => console.error(error));
 });
